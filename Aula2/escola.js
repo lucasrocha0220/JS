796d97848ff1313f3a063dea96443d2af9a0551a
lucasrocha0220 <lucasrochade8@gmail.com>
2026-04-01 16:51:49 -0300

    Add files via upload

---
+++ Aula2/escola.js
@@ -0,0 +1,27 @@
+// Aprendendo Funções e Condicionais
+function calcularMedia() {
+    var inNome = document.getElementById("inNome");
+    var inNota1 = document.getElementById("inNota1");
+    var  inNota2 = document.getElementById("inNota2");
+    var outSituacao = document.getElementById("outSituacao");
+      var outMedia = document.getElementById("outMedia");
+
+    var nome = inNome.value;
+    var nota1 = Number(inNota1.value);
+    var nota2 = Number(inNota2.value);
+
+    var media = (nota1 + nota2) / 2;
+
+    outMedia.textContent = "Média das Notas: " + media.toFixed(1);
+
+    if (media >= 6) {
+        outSituacao.textContent = "Parabéns  " + nome + " Você foi aprovado(a)";
+        outSituacao.style.color = "blue";
+    } else {
+        outSituacao.textContent = "Ops  " + nome + " ... Você foi reprovado(a)";
+        outSituacao.style.color = "red";
+    }
+}
+
+var btResultado = document.getElementById("btResultado");
+btResultado.addEventListener("click", calcularMedia);
